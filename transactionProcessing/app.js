ca2ef05300e99c415847f940cfd948fbd8ff54dd
Mikhail <pmp@nopreset.ru>
2022-08-26 20:36:58 -0400

    typo

--- transactionProcessing/app.js
+++ transactionProcessing/app.js
@@ -3,7 +3,7 @@ console.log("Loading function")
 exports.lambda_handler = async(event) => {
     // console.log("receive:", JSON.stringify(event,null,2))
     for (const { messageId, body} of event.Records){
-        console.log("SQS message is %s: %", messageId, body);
+        console.log("SQS message is %s:", messageId, body);
     }
-    return `Successfully processed ${event.Recrods.length} messages.`
+    return `Successfully processed ${event.Records.length} messages.`
 }
