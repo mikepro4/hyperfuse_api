43ab6fe9ba3ae51bcd5eed1e8db33bc0b8710c31
Mikhail <pmp@nopreset.ru>
2022-08-26 20:26:39 -0400

    sqs queue

---
+++ transactionProcessing/app.js
@@ -0,0 +1,9 @@
+console.log("Loading function")
+
+exports.lambda_handler = async(event) => {
+    // console.log("receive:", JSON.stringify(event,null,2))
+    for (const { messageId, body} of event.Records){
+        console.log("SQS message is %s: %", messageId, body);
+    }
+    return `Successfully processed ${event.Recrods.length} messages.`
+}
